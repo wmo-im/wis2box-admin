8ac326cdd709b4cc9d0d5419af48c2ad5f4c9431
charlesrc019 <30779702+charlesrc19@users.noreply.github.com>
2023-03-06 19:05:09 +0000

    Edited MCF form.

--- src/components/forms/McfForm.vue
+++ src/components/forms/McfForm.vue
@@ -5,12 +5,16 @@
         <v-toolbar-title v-html="$t(`datasets.mcf`)" />
 
         <v-spacer />
-        <v-btn color="blue-grey" class="ma-2" title="Reset">
-          reset
+        <v-btn color="blue-grey" class="ma-2" title="Reset" @click="resetForm()">
+          reset 
           <v-icon>mdi-sync</v-icon>
         </v-btn>
-        <v-btn color="#009900" class="ma-2" title="Export">
+        <v-btn color="#FFA500" class="ma-2" title="Download" :disabled="!valid" @click="downloadForm()">
           export
+          <v-icon>mdi-arrow-down-bold-box-outline</v-icon>
+        </v-btn>
+        <v-btn color="#009900" class="ma-2" title="Submit" :disabled="!valid">
+          submit
           <v-icon>mdi-cloud-upload</v-icon>
         </v-btn>
       </v-toolbar>
@@ -21,6 +25,10 @@
           v-model="model"
           :schema="schema"
           :options="options"
+          @input="updateModel($event)" 
+          @change="updateModel($event)" 
+          @input-child="updateModel($event)" 
+          @change-child="updateModel($event)"
         />
       </v-form>
     </v-card>
@@ -33,10 +41,9 @@ import "@koumoul/vjsf/lib/VJsf.css";
 import "@koumoul/vjsf/lib/deps/third-party.js";
 import $RefParser from "@apidevtools/json-schema-ref-parser";
 import mergeAllOf from "json-schema-merge-allof";
-
 import { parse } from "yaml";
 import { clean } from "@/scripts/helpers.js";
-import mcf from "@/models/mcf.json";
+import mcf_schema from "@/models/mcf_schema.json";
 
 let baseURL = window.VUE_ADMIN_URL;
 
@@ -67,10 +74,13 @@ export default {
     //   // handle success
     //   self.model = response.data;
     // });
-    this.loadMCF();
+    this.loadForm();
   },
   methods: {
-    async loadMCF() {
+    log(item) {
+      console.log(item)
+    },
+    async loadForm() {
       this.loading = true;
       var self = this;
       var cc = this.topic.split(".")[0];
@@ -89,32 +99,66 @@ export default {
           console.log(error);
         });
 
-      $RefParser.dereference(mcf, (err, schema) => {
+      $RefParser.dereference(mcf_schema, (err, schema) => {
         if (err) {
           console.error(err);
         } else {
           this.title = schema.title;
 
           const mergedSchema = mergeAllOf(schema);
-          const parsedSchema = this.parseSchema("root", mergedSchema);
+          const parsedSchema = this.prepareSchema("root", mergedSchema);
 
           this.schema = Object.assign({}, parsedSchema);
           this.loading = false;
         }
       });
     },
-    parseSchema(title, node) {
+    resetForm() {
+      this.model = {}
+    },
+    downloadForm() {
+      alert(JSON.stringify(this.prepareModel(this.model)))
+      var element = document.createElement("a")
+      var content = encodeURI(JSON.stringify(this.prepareModel(this.model), null, 4))
+      element.href = "data:attachment/text," + content
+      element.target = "_blank"
+      element.download = this.model.wis2box.topic_hierarchy + ".json"
+      element.click()
+    },
+    updateModel($event) {
+      this.log($event)
+    },
+    prepareModel(model) {
+
+      model["mcf"] = { version: this.schema.version.toFixed(1) }
+
+      model["identification"]["keywords"]["default"]["keywords"] = model.identification.search_keywords
+      delete model.identification.search_keywords
+      model["identification"]["keywords"]["wmo"]["keywords"] = model.identification.wmo_keywords
+      model["identification"]["keywords"]["wmo"]["keywords_type"] = "theme"
+      model["identification"]["keywords"]["wmo"]["vocabulary"]["name"] = "WMO Category Code"
+      model["identification"]["keywords"]["wmo"]["vocabulary"]["url"] = "https://github.com/wmo-im/wcmp-codelists/blob/main/codelists/WMO_CategoryCode.csv"
+      delete model.identification.search_keywords
+
+      model["contact"]["pointOfContact"] = model.poc
+      delete model.poc
+      model["contact"]["distributor"] = model.distrib
+      delete model.distrib
+
+      return model
+    },
+    prepareSchema(title, node) {
       if (typeof node !== "object") {
         return node;
       } else if (node.length) {
         for (var i = 0; i < node.length; i++) {
-          this.parseSchema(`node-${i}`, node[i]);
+          this.prepareSchema(`node-${i}`, node[i]);
         }
         return node;
       } else {
         for (const [key, val] of Object.entries(node)) {
-          if (title === "properties") this.parseSchema(key, val);
-          else this.parseSchema("", val);
+          if (title === "properties") this.prepareSchema(key, val);
+          else this.prepareSchema("", val);
         }
       }
 
