7cceafe074069aa3c82d90c41aa47d89ba8665ce
charlesrc019 <30779702+charlesrc19@users.noreply.github.com>
2023-03-06 22:18:06 +0000

    Updated to allow JSON and YAML export.

--- src/components/forms/McfForm.vue
+++ src/components/forms/McfForm.vue
@@ -9,10 +9,29 @@
           reset 
           <v-icon>mdi-sync</v-icon>
         </v-btn>
-        <v-btn color="#FFA500" class="ma-2" title="Download" :disabled="!valid" @click="downloadForm()">
-          export
-          <v-icon>mdi-arrow-down-bold-box-outline</v-icon>
-        </v-btn>
+        <v-menu>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              color="#FFA500" 
+              class="ma-2" 
+              title="Download"
+              v-bind="attrs"
+              v-on="on"
+              :disabled="!valid"
+            >
+              export
+              <v-icon>mdi-arrow-down-bold-box-outline</v-icon>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item @click="downloadForm('json')">
+              <v-list-item-title>JSON</v-list-item-title>
+            </v-list-item>
+            <v-list-item @click="downloadForm('yaml')">
+              <v-list-item-title>YAML</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
         <v-btn color="#009900" class="ma-2" title="Submit" :disabled="!valid">
           submit
           <v-icon>mdi-cloud-upload</v-icon>
@@ -41,9 +60,9 @@ import "@koumoul/vjsf/lib/VJsf.css";
 import "@koumoul/vjsf/lib/deps/third-party.js";
 import $RefParser from "@apidevtools/json-schema-ref-parser";
 import mergeAllOf from "json-schema-merge-allof";
-import { parse } from "yaml";
+import { parse, stringify } from "yaml";
 import { clean } from "@/scripts/helpers.js";
-import mcf_schema from "@/models/mcf_schema.json";
+import mcf_schema from "@/models/mcf-form.json";
 
 let baseURL = window.VUE_ADMIN_URL;
 
@@ -116,36 +135,76 @@ export default {
     resetForm() {
       this.model = {}
     },
-    downloadForm() {
-      alert(JSON.stringify(this.prepareModel(this.model)))
+    downloadForm(format) {
+      var content = encodeURI(JSON.stringify(this.modulateModel(this.model), null, 4))
+      if (format === 'yaml') {
+        content = encodeURI(stringify(this.modulateModel(this.model), null, 4))
+      }
       var element = document.createElement("a")
-      var content = encodeURI(JSON.stringify(this.prepareModel(this.model), null, 4))
       element.href = "data:attachment/text," + content
       element.target = "_blank"
-      element.download = this.model.wis2box.topic_hierarchy + ".json"
+      element.download = this.model.wis2box.topic_hierarchy + "." + format
       element.click()
     },
     updateModel($event) {
-      this.log($event)
+      if ("fullKey" in $event) {
+        this.log($event.fullKey)
+      }
     },
-    prepareModel(model) {
+    modulateModel(input) {
 
-      model["mcf"] = { version: this.schema.version.toFixed(1) }
+      var output = {}
 
-      model["identification"]["keywords"]["default"]["keywords"] = model.identification.search_keywords
-      delete model.identification.search_keywords
-      model["identification"]["keywords"]["wmo"]["keywords"] = model.identification.wmo_keywords
-      model["identification"]["keywords"]["wmo"]["keywords_type"] = "theme"
-      model["identification"]["keywords"]["wmo"]["vocabulary"]["name"] = "WMO Category Code"
-      model["identification"]["keywords"]["wmo"]["vocabulary"]["url"] = "https://github.com/wmo-im/wcmp-codelists/blob/main/codelists/WMO_CategoryCode.csv"
-      delete model.identification.search_keywords
+      output["mcf"] = { version: this.schema.version.toFixed(1) }
 
-      model["contact"]["pointOfContact"] = model.poc
-      delete model.poc
-      model["contact"]["distributor"] = model.distrib
-      delete model.distrib
+      output["wis2box"] = input.wis2box
+      output["wis2box"]["retention"] =  `P${input.wis2box.retention.toUpperCase()}`
 
-      return model
+      output["metadata"] = input.metadata
+
+      output["identification"] = {}
+      output["identification"]["title"] = input.identification.title
+      output["identification"]["abstract"] = input.identification.abstract
+      output["identification"]["url"] = input.identification.url
+      output["identification"]["wmo_data_policy"] = input.identification.wmo_data_policy
+      output["identification"]["keywords"] = {}
+      output["identification"]["keywords"]["default"] = {}
+      output["identification"]["keywords"]["default"]["keywords"] = input.identification.search_keywords
+      output["identification"]["keywords"]["wmo"] = {}
+      output["identification"]["keywords"]["wmo"]["keywords"] = input.identification.wmo_keywords
+      output["identification"]["keywords"]["wmo"]["keywords_type"] = "theme"
+      output["identification"]["keywords"]["wmo"]["vocabulary"] = {}
+      output["identification"]["keywords"]["wmo"]["vocabulary"]["name"] = "WMO Category Code"
+      output["identification"]["keywords"]["wmo"]["vocabulary"]["url"] = "https://github.com/wmo-im/wcmp-codelists/blob/main/codelists/WMO_CategoryCode.csv"
+
+      output["contact"] = {}
+      output["contact"]["pointOfContact"] = input.poc
+      output["contact"]["distributor"] = input.distrib
+      delete output["contact"]["distributor"]["duplicate_info"]
+
+      return output
+    },
+    demodulateModel(input) {
+      
+      var output = {}
+
+      output["wis2box"] = input.wis2box
+      output["wis2box"]["retention"] =  `P${input.wis2box.retention.toUpperCase()}`
+
+      output["metadata"] = input.metadata
+
+      output["identification"] = {}
+      output["identification"]["title"] = input.identification.title
+      output["identification"]["abstract"] = input.identification.abstract
+      output["identification"]["url"] = input.identification.url
+      output["identification"]["wmo_data_policy"] = input.identification.wmo_data_policy
+      output["identification"]["search_keywords"] = input.identification.keywords.default.keywords
+      output["identification"]["wmo_keywords"] = input.identification.keywords.wmo.keywords
+
+      output["poc"] = input.contact.pointOfContact
+      output["distrib"] = input.contact.distributor
+      output["distrib"]["duplicate_info"] = false
+
     },
     prepareSchema(title, node) {
       if (typeof node !== "object") {
