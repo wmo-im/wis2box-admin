dd0cf5e3cd9ee73e37a3e9d720a85c0dfc0390b5
jmaxmarno <jmaxmarno@gmail.com>
2023-04-06 17:50:25 -0600

    location /

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = {
-    publicPath: "/admin",
+    publicPath: "/",
 
     transpileDependencies: ["vuetify", "@koumoul/vjsf"],
 
