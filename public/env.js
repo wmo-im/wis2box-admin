1ec85cc192ce0a3417c73c25f6f286a1b95702d9
Max Marno <9143887+jmaxmarno@users.noreply.github.com>
2023-04-28 08:10:00 -0600

    Update env.js

--- public/env.js
+++ public/env.js
@@ -1,9 +1,4 @@
 // env.js
-// store env variables on start of docker image
-window.MQTT_HOST='mosquitto'
 window.VUE_APP_URL='http://localhost'
 window.VUE_APP_OAPI='http://localhost/oapi'
-window.VUE_ADMIN_URL='http://localhost/admin'
-window.VUE_APP_BASEMAP_URL='https://\{s\}.tile.openstreetmap.org/\{z\}/\{x\}/\{y\}.png'
-window.VUE_APP_BASEMAP_ATTRIBUTION='$&copy; <a href=\"https://osm.org/copyright\">OpenStreetMap</a> contributors'
 window.VUE_ADMIN_LOC = ""
